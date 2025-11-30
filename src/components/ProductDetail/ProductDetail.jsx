731f1e4e49a3e5c75d5ae4e598b9fb609bd98fa5
chimpa27 <dasilva.an97@gmail.com>
2025-11-30 09:25:42 -0300

    useEffect y useState

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,12 +1,12 @@
 import "./ProductDetail.css"
 import { ShoppingCart } from "lucide-react"
 
-function ProductDetail (){
+function ProductDetail ({product}){
     return(
         <div className="productDetail">
-            <h3>Nombre</h3>
-            <p>descripcion</p>
-            <span>precio $</span>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <span>{product.price} $</span>
             <button className="btn-cart">
                 <ShoppingCart size={32} strokeWidth={1.75} />
                 Carrito
--- src/components/ProductListContainer/ProductListContainer.jsx
+++ src/components/ProductListContainer/ProductListContainer.jsx
@@ -1,15 +1,34 @@
-import ProductDetail from "../ProductDetail/ProductDetail"
-import "./ProductListContainer.css"
+import { useEffect } from "react";
+import ProductDetail from "../ProductDetail/ProductDetail";
+import "./ProductListContainer.css";
+import { useState } from "react";
 
-function ProductListContainer(){
-    
-    return(
-        <div className="productListContainer">
-            <ProductDetail/>
-            <ProductDetail/>
-            <ProductDetail/>
-        </div>
-    )
+function ProductListContainer() {
+
+    const [products,setProducts] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch("src/JSONS/products.json");
+                const data = await response.json()
+                setProducts(data)
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, []);
+
+  return (
+    <div className="productListContainer">
+        {
+            products.map((product)=>{
+                return <ProductDetail key={product.id} product={product}/>
+            })
+
+        }
+    </div>
+  );
 }
 
-export default ProductListContainer
+export default ProductListContainer;
